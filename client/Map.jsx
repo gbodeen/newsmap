97bb9f61e283f8fea0a90ba865fda90a9ec51f27
Gabriel Bodeen <gabebodeen@gmail.com>
2019-02-21 17:09:25 -0600

    Use custom markers w/ round bounders & pointer. Merged & squashed from custommarkers.

--- client/Map.jsx
+++ client/Map.jsx
@@ -1,21 +1,10 @@
 import React, { useEffect, useState } from 'react';
-// import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-// import apiKey from './secrets/api.js';
+import { withScriptjs, withGoogleMap, GoogleMap, OverlayView, Marker } from "react-google-maps"
+import CustomMarker from './Marker.jsx';
 
 
 const Map = withScriptjs(withGoogleMap((props) => {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     news: []
-  //   }
-  // }
   const [news, setNews] = useState([]);
-
-  // componentDidMount() {
-  //   this.getNews();
-  // }
 
   const getNews = () => {
     fetch('news')
@@ -25,32 +14,13 @@ const Map = withScriptjs(withGoogleMap((props) => {
 
   useEffect(getNews, []);
 
-  const onMarkerClick = (url) => {
-    window.open(url);
-  }
-
   return (
     <GoogleMap
       defaultZoom={5}
       center={{ lat: 38.00000, lng: -96.00000 }}
     >
-
       {news.map(story => {
-        return (
-          <Marker
-            title={story.description}
-            name={story.title}
-            position={story.geocode}
-            onClick={() => onMarkerClick(story.url)}
-            class="customMarker"
-            key={Math.random()}
-            icon={{
-              url: story.urlToImage,
-              anchor: new google.maps.Point(24, 24),
-              scaledSize: new google.maps.Size(48, 48)
-            }}
-          />
-        )
+        return <CustomMarker story={story} key={Math.random()} />;
       })}
     </GoogleMap>
   )
