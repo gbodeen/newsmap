df43e9c084a12068574df7c52c36618738250918
Gabriel Bodeen <gabebodeen@gmail.com>
2019-02-23 17:28:55 -0600

    Permit clearOldImages() to happen asynch.

--- server/imageAssets.js
+++ server/imageAssets.js
@@ -2,32 +2,27 @@ const fs = require('fs');
 require('dotenv').config();
 const imagePath = process.env.IMAGE_PATH;
 
-const clearOldImages = () => new Promise((resolve, reject) => {
-  fs.readdir(imagePath, (err, files) => {
+const clearOldImages = (path = imagePath) => new Promise((resolve, reject) => {
+  // clearOldImages is required to return a promise so that it completes 
+  // before saveImages writes any new ones; alternatively, it could acquire
+  // its file list and then delete only those, while saveImages writes any new images
+  // in whatever order it gets them. (But what if the images have the same name? Oh,
+  // give the file a random name and add that to the story.)
+  fs.readdir(path, (err, files) => {
     if (err) {
-      console.log(`Failed to read ${imagePath}.`);
+      console.log(`Failed to read ${path}.`);
       reject(err);
     } else {
-      Promise.all(files.map(file =>
-        new Promise((resolve, reject) => {
-          fs.unlink(imagePath + '/' + file, (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve();
-            }
-          });
-        })
-      ))
-        .then(success => resolve(success))
-        .catch(err => reject(err));
+      files.forEach(file => fs.unlink(path + '/' + file, (delErr) => {
+        if (delErr) console.log(`Error deleting ${file}.`);
+      }));
+      resolve('Deleting files now.');
     }
   });
 });
 
 const saveImages = async (articles) => {
   await clearOldImages();
-
 }
 
 clearOldImages();
