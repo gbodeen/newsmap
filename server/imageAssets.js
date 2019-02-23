56467a7db52bf924e1f8056e486848ccf624f67c
Gabriel Bodeen <gabebodeen@gmail.com>
2019-02-23 17:52:23 -0600

    In progress: Download & save URL of article main image.

--- server/imageAssets.js
+++ server/imageAssets.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 require('dotenv').config();
 const imagePath = process.env.IMAGE_PATH;
+const http = require('http');
 
 const clearOldImages = (path = imagePath) => new Promise((resolve, reject) => {
   // clearOldImages is required to return a promise so that it completes 
@@ -21,10 +22,34 @@ const clearOldImages = (path = imagePath) => new Promise((resolve, reject) => {
   });
 });
 
-const saveImages = async (articles) => {
-  await clearOldImages();
+const saveImages = (articles, path = imagePath) => {
+  clearOldImages();
+  console.log(articles);
+  return Promise.all(articles.map(article => new Promise((resolve, reject) => {
+    console.log('this ran');
+    // find image URL
+    let url = article.urlToImage;
+    let extension = url.match(/\.\w{3,4}$/g)[0];
+    // download image && save to file with random name 
+    const filename = Math.random().toString().slice(2) + extension;
+    const file = fs.createWriteStream(path + '/' + filename);
+    const request = http.get(url, res => {
+      res.pipe(file);
+      file.on('finish', () => file.close()) // might need cb
+    })
+      .on('error', err => { // Handle errors
+        fs.unlink(path + '/' + filename); // Delete the file async. (But we don't check the result)
+        console.log(`Error ${err} writing ${file} for ${url}.`);
+        reject('Failed to write file.');
+      });
+    resolve();
+    // attach filename to article
+    // resolve if success
+    // reject if failure at any step
+  })));
 }
 
-clearOldImages();
+let test = { urlToImage: 'http://localhost:3000/images/blackpointer.png' };
+saveImages([test]);
 
 module.exports = saveImages;
